ce6780b07d6473132c25b2cf29441a72f88b8561
Walter Filho <walterfilho1000@gmail.com>
2023-03-16 01:18:44 -0300

    atividade

---
+++ js/repositorio/disciplina-repositorio.js
@@ -0,0 +1,27 @@
+class DisciplinaRepositorio {
+    constructor() {
+        this._disciplinas = [];
+    }
+
+    inserir(disciplina) {
+        this._disciplinas.push(disciplina);
+        return disciplina;
+    }
+
+    pesquisar(codigo) {
+        return this._disciplinas.find(disciplina => disciplina.codigo === codigo);
+    }
+
+    remover(codigo) {
+        const indxRemocao = this._disciplinas.findIndex(disciplina => disciplina.codigo === codigo);
+        this._disciplinas.splice(indxRemocao, 1);
+    }
+
+    listar() {
+        return this._disciplinas;
+    }
+
+    buscarPorNome(nome) {
+        return this._disciplinas.filter(disciplina => disciplina.nome === nome);
+    }
+}
---
+++ js/servico/disciplina-servico.js
@@ -0,0 +1,23 @@
+class DisciplinaServico {
+    constructor() {
+        this._repositorio = new DisciplinaRepositorio();
+    }
+
+    inserir(nome, codigo) {
+        const disciplina = new Disciplina(nome, codigo);
+        return this._repositorio.inserir(disciplina);
+    }
+    
+    remover(codigo) {     
+        this._repositorio.remover(codigo);
+    }
+
+    listar() {
+        return this._repositorio.listar();
+    }
+
+    buscarPorCodigo(codigo) {
+        return this._repositorio.buscarPorNome(codigo);
+    }   
+}
+   
